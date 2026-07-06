a44b7a278800229211f07c5f2a90a457e7ab658f
Tyrone Santos <tyrnesantos04@gmail.com>
2026-07-07 01:30:15 +0800

    Added Candudate page on admin/hr with department filter

--- hr/urls.py
+++ hr/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     path("login/", views.hr_login, name="hr_login"),
     path("", views.dashboard, name="dashboard"),
+    
     path("jobs/", views.job_management, name="job_management"),
     path("jobs/create/", views.create_job, name="create_job"),
     path("jobs/<int:pk>/", views.manage_job, name="manage_job"),
+    
+    path("candidates/", views.candidates, name="candidates"),
+    path("candidates/<str:department>/",views.candidate_department, name="candidate_department",),
 ]
