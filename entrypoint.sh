1ca28b038a6701a90c61e4b5520656e64f4a7f79
Kurokocchii <mntnacc0@gmail.com>
2026-07-26 14:34:30 +0800

    Update entrypoint script to use dynamic database host and port variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,13 @@
 
 echo "Waiting for MySQL..."
 
-while ! nc -z database 3306; do
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
     sleep 1
 done
 
 echo "MySQL is ready."
 
 python manage.py migrate --noinput
-
 python manage.py collectstatic --noinput
 
 exec "$@"
